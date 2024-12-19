f9478e546254156cfd7d793733e8e9db87f000bb
Jonathan Luciano <129464379+JonathanLuher@users.noreply.github.com>
2024-12-18 23:45:02 -0600

    Practice 6 and 7
    In this commit I puloaded the practice report and code of the practices 6 and 7

---
+++ practica 7/practica7/practica7.ino
@@ -0,0 +1,78 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal.h>
+
+// Pines del lector RFID
+#define SS_PIN 10
+#define RST_PIN 7
+
+// Pines del LCD
+LiquidCrystal lcd(8, 9, 5, 4, 3, 2);
+
+// Instancia del lector RFID
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+// UID autorizado (73 B8 96 A1)
+byte uidAutorizado[] = {0xE3, 0xF3, 0x19, 0x14};
+
+void setup() {
+  // Inicialización del LCD
+  lcd.begin(16, 2); // Configuración del LCD de 16x2
+  lcd.print("CERRADO"); // Mensaje inicial
+
+  // Inicialización del Serial
+  Serial.begin(9600);
+
+  // Inicialización del SPI y el lector RFID
+  SPI.begin();
+  rfid.PCD_Init();
+
+  Serial.println("Lector listo.");
+}
+
+void loop() {
+  // Verifica si hay una nueva tarjeta
+  if (!rfid.PICC_IsNewCardPresent()) return;
+  if (!rfid.PICC_ReadCardSerial()) return;
+
+  // Comparar el UID leído con el UID autorizado
+  if (verificarUID()) {
+    // Tarjeta autorizada
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("BIENVENIDO");
+    lcd.setCursor(0, 1);
+    lcd.print("Acceso autorizado");
+
+    Serial.println("Acceso autorizado.");
+  } else {
+    // Tarjeta no autorizada
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Fuera!");
+    lcd.setCursor(0, 1);
+    lcd.print("Acceso denegado");
+
+    Serial.println("Acceso denegado.");
+  }
+
+  // Espera unos segundos antes de volver al estado inicial
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("CERRADO");
+
+  // Detiene la comunicación con la tarjeta
+  rfid.PICC_HaltA();
+}
+
+// Función para verificar si el UID leído coincide con el autorizado
+bool verificarUID() {
+  if (rfid.uid.size != sizeof(uidAutorizado)) return false;
+
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    if (rfid.uid.uidByte[i] != uidAutorizado[i]) return false;
+  }
+
+  return true; // Todos los bytes coinciden
+}
